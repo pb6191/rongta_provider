0d23b7fda5cbbed12b2fa8ecb5d42f5be1e105de
user273648 <user273648@#13.109>
2025-02-08 10:25:02 -0500

    ios ok

--- native-mobile-app-shell-master-provider/ios/Flutter/flutter_export_environment.sh
+++ native-mobile-app-shell-master-provider/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\PC-P14s\Downloads\flutter_windows_3.27.1-stable\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\PC-P14s\Desktop\native-mobile-app-shell-master-provider\native-mobile-app-shell-master-provider"
+export "FLUTTER_ROOT=/Users/user273648/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user273648/Documents/rongta_provider/native-mobile-app-shell-master-provider"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=5"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
