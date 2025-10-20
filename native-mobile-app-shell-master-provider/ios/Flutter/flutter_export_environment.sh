ef7bad2f041193dc1b4af1d108e506aea1e19b64
puneet bhargava <pb6191@gmail.com>
2025-10-20 18:39:59 -0400

    publish ntah

--- native-mobile-app-shell-master-provider/ios/Flutter/flutter_export_environment.sh
+++ native-mobile-app-shell-master-provider/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/user273677/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user273677/Documents/rongta_provider/native-mobile-app-shell-master-provider"
+export "FLUTTER_ROOT=/Users/user282010/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user282010/Documents/rongta_provider/native-mobile-app-shell-master-provider"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=6"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
